a39f0c562beb303051266c332101438fdae0110e
dlgs5100 <dlgs5100@gmail.com>
2018-11-07 18:20:56 +0800

    complete draw three point convex hull and perpendicular Bisector

--- MainWindow.py
+++ MainWindow.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.uic import loadUi
 import MessageDialog as MessageDialog
 
+
 class GraphicsScene(QtWidgets.QGraphicsScene):
     moved = QtCore.pyqtSignal(QtWidgets.QGraphicsSceneMouseEvent)
     def __init__(self, parent=None):
@@ -21,14 +22,20 @@ class GraphicsScene(QtWidgets.QGraphicsScene):
             self.addEllipse(x, y, 1, 1, pen, brush)
         
             MainWindow.listPoint.append(point)
+            MainWindow.listPointCount[0] += 1
             # Testing
             print(x, y)
+            print(MainWindow.listPointCount)
 
     def mouseMoveEvent(self, event):
         self.moved.emit(event)
 
 class MainWindow(QtWidgets.QMainWindow):
     listPoint = []
+    listPointCount = [0]
+    listConvexLine = []
+    listPerpendicularBisector = []
+    
     def __init__(self):
         super(MainWindow,self).__init__()
         loadUi('MainWindow.ui', self)
@@ -36,7 +43,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.buttonSet.clicked.connect(self.listenerSet)
         # self.buttonStep.clicked.connect(self.listenerStep)
         self.buttonInput.clicked.connect(self.listenerInput)
-        # self.buttonRun.clicked.connect(self.listenerRun)
+        self.buttonRun.clicked.connect(self.listenerRun)
         # self.buttonOutput.clicked.connect(self.listenerOutput)
         self.buttonClear.clicked.connect(self.listenerClear)
 
@@ -61,6 +68,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 brush = QtGui.QBrush(QtCore.Qt.red)
                 self.scene.addEllipse(point[0], point[1], 1, 1, pen, brush)
                 self.listPoint.append(point)
+                self.listPointCount[0] += 1
                     
                 self.graphicsView.show()
 
@@ -72,9 +80,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def listenerClear(self):
         self.listPoint.clear()
+        self.listPointCount.clear()
         self.scene.clear()
+
+        self.listPointCount.append(0)
     
     def listenerInput(self):
+        self.listPointCount.clear()
         options = QtWidgets.QFileDialog.Options()
         options |= QtWidgets.QFileDialog.DontUseNativeDialog
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
@@ -84,15 +96,59 @@ class MainWindow(QtWidgets.QMainWindow):
             f = open(fileName, 'r')
 
             while 1:
-                size = int(f.readline())
-                if size > 0:
-                    for i in range (0, size):
-                        point = f.readline().split()
-                        point = list(map(int, point))
-                        self.listPoint.append(point)
-                else:
-                    print(size)
-                    break
+                line = f.readline()
+                line = line[:-1]
+                if line.isdigit():
+                    size = int(line)
+                    if size > 0:
+                        self.listPointCount.append(size)
+                        for i in range (0, size):
+                            point = f.readline().split()
+                            point = list(map(int, point))
+                            self.listPoint.append(point)
+                    else:
+                        break
 
-    def draw(self):
-        print(123)
+    def listenerRun(self):
+        self.listConvexLine.append(QtCore.QLineF(self.listPoint[0][0], self.listPoint[0][1], self.listPoint[1][0], self.listPoint[1][1]))
+        self.listConvexLine.append(QtCore.QLineF(self.listPoint[1][0], self.listPoint[1][1], self.listPoint[2][0], self.listPoint[2][1]))
+        self.listConvexLine.append(QtCore.QLineF(self.listPoint[2][0], self.listPoint[2][1], self.listPoint[0][0], self.listPoint[0][1]))
+
+        # convex hull
+        for i in range(0, 3):
+            self.drawConvex(self.listConvexLine[i])
+
+        # Perpendicular Bisector
+        self.drawPerpendicularBisector(self.listPoint[0][0], self.listPoint[0][1], self.listPoint[1][0], self.listPoint[1][1])
+        self.drawPerpendicularBisector(self.listPoint[1][0], self.listPoint[1][1], self.listPoint[2][0], self.listPoint[2][1])
+        self.drawPerpendicularBisector(self.listPoint[2][0], self.listPoint[2][1], self.listPoint[0][0], self.listPoint[0][1])
+
+        # Erase
+        temp = QtCore.QPointF(0,0)
+        # print(self.listPerpendicularBisector[0].intersect(self.listPerpendicularBisector[0], temp))
+        self.listPerpendicularBisector[0].intersect(self.listPerpendicularBisector[1], temp)
+        print(temp)     # Find the point
+
+    def drawConvex(self, line):
+        self.pen = QtGui.QPen(QtCore.Qt.green)
+        # 3 point
+        self.scene.addLine(line, self.pen)
+
+    def drawPerpendicularBisector(self, x1, y1, x2, y2):
+        midpointX = (x1+x2)/2
+        midpointY = (y1+y2)/2
+        # Perpendicular Bisector slope
+        if (x2-x1) != 0:
+            m = -(x2-x1)/(y2-y1)
+        else:
+            m = 0
+        c = midpointY-(m*midpointX)
+        
+        y1New = m*0+c
+        y2New = m*600+c
+    
+        self.pen = QtGui.QPen(QtCore.Qt.blue)
+        line = QtCore.QLineF(0, y1New, 600, y2New)
+        self.scene.addLine(line, self.pen)
+
+        self.listPerpendicularBisector.append(line)
