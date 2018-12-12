32a2cea1a845db69ecd3864d90a5328de75a9e8a
dlgs5100 <dlgs5100@gmail.com>
2018-12-12 17:08:31 +0800

    README update

--- MainWindow.py
+++ MainWindow.py
@@ -334,8 +334,6 @@ class MainWindow(QtWidgets.QMainWindow):
                     position = self.determineIntersectionRelativePosition(listLocalConvexLine[i][0], intersectionPoint)
                     listLocalConvexLine[i].append(position)
                     self.deleteExceedLine(listLocalConvexLine[i], intersectionPoint)
-            
-            self.resultLine = self.sortLine()
 
             #存convex中垂線
             temp = []
